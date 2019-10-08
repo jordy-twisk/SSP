8ee26f85bd4f20a9ffab917156018cd88e316f57
Barend <moschbarend@gmail.com>
2019-10-08 17:03:29 +0200

    added PUT call through directly access the JSON

--- User.cs
+++ User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,6 +9,7 @@ namespace TinderCloneV1{
             get;
             set;
         }
+        [JsonProperty("firstName")]
         public string firstName{
             get;
             set;
--- UserController.cs
+++ UserController.cs
@@ -13,6 +13,9 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Net;
 using System.Text;
+using com.sun.org.apache.bcel.@internal.generic;
+using System.Reflection;
+using Newtonsoft.Json.Linq;
 
 namespace TinderCloneV1{
     public static class UserController{
@@ -32,18 +35,14 @@ namespace TinderCloneV1{
                 using (SqlConnection connection = new SqlConnection(str)){
                     connection.Open();
 
-                    if (req.Method == HttpMethod.Get)
-                    {
+                    if (req.Method == HttpMethod.Get){
+                        User getStudent = new User();
                         queryString = $"SELECT * FROM [dbo].[Student] WHERE studentID = {studentID};";
 
-                        using (SqlCommand command = new SqlCommand(queryString, connection))
-                        {
-                            using (SqlDataReader reader = command.ExecuteReader())
-                            {
-                                while (reader.Read())
-                                {
-                                    student.Add(new User
-                                    {
+                        using (SqlCommand command = new SqlCommand(queryString, connection)){
+                            using (SqlDataReader reader = command.ExecuteReader())  {
+                                while (reader.Read())  {
+                                   getStudent = new User  {
                                         studentID = reader.GetInt32(0),
                                         firstName = reader.GetString(1),
                                         surName = reader.GetString(2),
@@ -54,27 +53,55 @@ namespace TinderCloneV1{
                                         study = reader.GetString(7),
                                         studyYear = reader.GetInt32(8),
                                         interests = reader.GetString(9)
-                                    });
+                                    };
                                 }
                             }
                         }
-                        var jsonToReturn = JsonConvert.SerializeObject(student);
+                        var jsonToReturn = JsonConvert.SerializeObject(getStudent);
                         log.Info($"{HttpStatusCode.OK} | Data shown succesfully");
-                        return new HttpResponseMessage(HttpStatusCode.OK)
-                        {
+
+                        return new HttpResponseMessage(HttpStatusCode.OK){
                             Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
                         };
-                    }
-                    else if (req.Method == HttpMethod.Put) { 
-                        
-                    }
+                    } 
+                    else if (req.Method == HttpMethod.Put) {
+                        string body = await req.Content.ReadAsStringAsync();
+                        //User newDataStudent = new User();
+                        JObject jObject = new JObject();
+                        List<String> propertyNames = new List<String>();
 
-                    else if (req.Method == HttpMethod.Post){
+                        using (StringReader reader = new StringReader(body)) {
+                            string json = reader.ReadToEnd();
+                            jObject = JsonConvert.DeserializeObject<JObject>(json);
+                            //newDataStudent = jObject.ToObject<User>();
+                        }
+
+                        foreach(JProperty property in jObject.Properties()) {
+                            queryString = $"UPDATE [dbo].[Student]" +
+                                $"SET {property.Name} = '{property.Value}'" +
+                                $"WHERE studentID = {studentID};";
+
+                            SqlCommand commandUpdate = new SqlCommand(queryString, connection);
+                            await commandUpdate.ExecuteNonQueryAsync();
+                        }
+                        
+                        /*
+                        New querystring for every column you want to change instead of changing multiple column at once
+                        This is because we do not know how many columns the user wants to change, hence the foreach loop
+                        */
+
+                        /* queryString = $"UPDATE [dbo].[Student] " +
+                            $"SET firstName = '', surName = '', phoneNumber = '', photo = '', description = '', degree = '', study = '', studyear = , interests = ''" +
+                            $"WHERE studentID = {studentID};"; */
+
+                        
+                        HttpResponseMessage = req.CreateResponse(HttpStatusCode.OK, $" Changed data of student: {studentID}");
+                    } 
+                    else if (req.Method == HttpMethod.Post) {
                         string body = await req.Content.ReadAsStringAsync();
                         int i = student.Count;
 
-                        using (StringReader reader = new StringReader(body))
-                        {
+                        using (StringReader reader = new StringReader(body)) {
                             string json = reader.ReadToEnd();
                             student.Add(JsonConvert.DeserializeObject<User>(json));
                         }
@@ -89,26 +116,22 @@ namespace TinderCloneV1{
                         await command.ExecuteNonQueryAsync();
 
                         HttpResponseMessage = req.CreateResponse(HttpStatusCode.OK, $" {studentID} Added to the database");
-                    }
-                    else if (req.Method == HttpMethod.Delete)
-                    {
+                    } 
+                    else if (req.Method == HttpMethod.Delete){
                         queryString = $"DELETE FROM [dbo].[Student] WHERE {studentID} = studentID;";
 
                         SqlCommand command = new SqlCommand(queryString, connection);
                         await command.ExecuteNonQueryAsync();
 
                         HttpResponseMessage = req.CreateResponse(HttpStatusCode.OK, $"{studentID} Deleted from the database");
-                    }
-
-                    else
-                    {
+                    } 
+                    else {
                         HttpResponseMessage = req.CreateResponse(HttpStatusCode.NotFound, $"HTTP Trigger not initialized, Make sure to use GET, POST, DELETE, PUT");
                     }
 
                     return HttpResponseMessage;
                 }
-            }
-            catch (SqlException e){
+            } catch (SqlException e){
                 log.Info(e.Message);
                 return req.CreateResponse($"HttpStatusCode.BadRequest, The following SqlException happened: {e.Message}");
             }
