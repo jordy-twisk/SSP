8b467d1b3f4903848d33c5ceaa79ff8103342a53
Sinscerly <email.thomas.jansema@gmail.com>
2019-10-17 13:29:02 +0200

    try catch for database connection

--- src/cs/controllers/UsersController.cs
+++ src/cs/controllers/UsersController.cs
@@ -13,12 +13,16 @@ using System.Net;
 using System.Text;
 using System.Reflection;
 
-namespace TinderCloneV1{
-    public static class UsersController{
+namespace TinderCloneV1
+{
+    public static class UsersController
+    {
         [FunctionName("getUsers")]
         [Obsolete]
-        public static async Task<HttpResponseMessage> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, 
-            "get", Route = "students/search")] HttpRequestMessage req, HttpRequest request, TraceWriter log){
+        public static async Task<HttpResponseMessage> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous,
+            "get", Route = "students/search")] HttpRequestMessage req, HttpRequest request, TraceWriter log)
+        {
+            ExceptionHandler exception = new ExceptionHandler(0);
 
             string str = Environment.GetEnvironmentVariable("sqldb_connection");
             List<User> listOfUsers = new List<User>();
@@ -27,23 +31,38 @@ namespace TinderCloneV1{
 
             PropertyInfo[] properties = typeof(User).GetProperties();
 
-            using (SqlConnection connection = new SqlConnection(str)){
-                connection.Open();
-                try{
+            using (SqlConnection connection = new SqlConnection(str))
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch (SqlException e)
+                {
+                    log.Info(e.Message);
+                    return exception.ServiceUnavailable(log);
+                }
+                try
+                {
 
                     queryString = $"SELECT * FROM [dbo].[Student]";
 
                     int i = 0;
-                    foreach(PropertyInfo p in properties){ 
-                        if (i == 0){
+                    foreach (PropertyInfo p in properties)
+                    {
+                        if (i == 0)
+                        {
                             queryString += $" WHERE";
                         }
 
-                        if(request.Query[p.Name] != isEmpty){
-                            if (p.Name == "interests" || p.Name == "study"){
+                        if (request.Query[p.Name] != isEmpty)
+                        {
+                            if (p.Name == "interests" || p.Name == "study")
+                            {
                                 queryString += $" {p.Name} LIKE '%{request.Query[p.Name]}%' AND";
                             }
-                            else {
+                            else
+                            {
                                 queryString += $" {p.Name} = '{request.Query[p.Name]}' AND";
                             }
                         }
@@ -56,20 +75,24 @@ namespace TinderCloneV1{
 
                     log.Info($"Executing the following query: {queryString}");
 
-                    using (SqlCommand command = new SqlCommand(queryString, connection)){
-                        using (SqlDataReader reader = command.ExecuteReader()){
-                            while (reader.Read()){
-                                listOfUsers.Add(new User{
+                    using (SqlCommand command = new SqlCommand(queryString, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                listOfUsers.Add(new User
+                                {
                                     studentID = reader.GetInt32(0),
-                                        firstName = reader.GetString(1),
-                                        surName = reader.GetString(2),
-                                        phoneNumber = reader.GetString(3),
-                                        photo = reader.GetString(4),
-                                        description = reader.GetString(5),
-                                        degree = reader.GetString(6),
-                                        study = reader.GetString(7),
-                                        studyYear = reader.GetInt32(8),
-                                        interests = reader.GetString(9)
+                                    firstName = reader.GetString(1),
+                                    surName = reader.GetString(2),
+                                    phoneNumber = reader.GetString(3),
+                                    photo = reader.GetString(4),
+                                    description = reader.GetString(5),
+                                    degree = reader.GetString(6),
+                                    study = reader.GetString(7),
+                                    studyYear = reader.GetInt32(8),
+                                    interests = reader.GetString(9)
                                 });
                             }
                         }
@@ -78,12 +101,14 @@ namespace TinderCloneV1{
                     var jsonToReturn = JsonConvert.SerializeObject(listOfUsers);
                     log.Info($"{HttpStatusCode.OK} | Data shown succesfully");
 
-                    return new HttpResponseMessage(HttpStatusCode.OK){
+                    return new HttpResponseMessage(HttpStatusCode.OK)
+                    {
                         Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
                     };
 
                 }
-                catch (SqlException e){
+                catch (SqlException e)
+                {
                     log.Info(e.StackTrace);
                     return req.CreateResponse(HttpStatusCode.BadRequest, $"The following SqlException happened: {e.StackTrace}");
                 }
