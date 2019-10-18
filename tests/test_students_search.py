8fb0fbc3adfccea96b289c7ab47f7c16e133e260
Thomas_Jansema_Aka_Sinscerly <Sinscerly@users.noreply.github.com>
2019-10-18 17:42:44 +0200

    added students_search to pytest

---
+++ tests/test_students_search.py
@@ -0,0 +1,12 @@
+import pytest
+import requests
+import os
+import api_info as a
+sup_url = a.api_link()
+
+
+def test_get_student():
+  print("test_get_student")
+  r = requests.get(a.api_link() + "students/search")
+  assert r.status_code == 200, r.status_code
+
