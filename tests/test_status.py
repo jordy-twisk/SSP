9d4fb075a6bc3fb0f30420860c6a4230b23cc90d
Thomas_Jansema_Aka_Sinscerly <Sinscerly@users.noreply.github.com>
2019-10-18 18:11:11 +0200

    added status url, added py.test script.

--- tests/test_status.py
+++ tests/test_status.py
@@ -5,7 +5,7 @@ import api_info as a
 sup_url = a.api_link()
 
 def test_get_student():
-  print("test_get_student")
-  r = requests.get(a.api_link() + "students/search")
+  print("test_status")
+  r = requests.get(a.api_link() + "status")
   assert r.status_code == 200, r.status_code
 
--- tests/test_student_ID.py
+++ tests/test_student_ID.py
@@ -4,7 +4,6 @@ import os
 import api_info as a
 sup_url = a.api_link()
 
-
 def test_get_student():
   print("test_get_student")
   r = requests.get(a.api_link() + "student/581433")
