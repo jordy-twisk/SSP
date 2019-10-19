c4a97e6faddc65b0983827964f1323ce0e77707f
Thomas_Jansema_Aka_Sinscerly <Sinscerly@users.noreply.github.com>
2019-10-19 22:47:01 +0200

    added skeleten for further api calls, filled in CoachController.

--- src/cs/controllers/StudentController.cs
+++ src/cs/controllers/StudentController.cs
@@ -9,22 +9,27 @@ using Microsoft.Extensions.Logging;
 
 namespace TinderCloneV1
 {
-    public class StudentController{
+    public class StudentController
+    {
         string str = Environment.GetEnvironmentVariable("sqldb_connection");
         ExceptionHandler exceptionHandler = new ExceptionHandler(0);
         Task<HttpResponseMessage> httpResponseMessage = null;
 
         [FunctionName("GetUsers")]
         public async Task<HttpResponseMessage> GetUsers([HttpTrigger(AuthorizationLevel.Anonymous,
-            "get", Route = "students/search")] HttpRequestMessage req, HttpRequest request, ILogger log){
-            
+            "get", Route = "students/search")] HttpRequestMessage req, HttpRequest request, ILogger log)
+        {
+
             UserService userService = new UserService(req, request, log);
 
-            using (SqlConnection connection = new SqlConnection(str)){
-                try {
+            using (SqlConnection connection = new SqlConnection(str))
+            {
+                try
+                {
                     connection.Open();
                 }
-                catch (SqlException e) {
+                catch (SqlException e)
+                {
                     log.LogInformation(e.Message);
                     return exceptionHandler.ServiceUnavailable(log);
                 }
@@ -39,34 +44,40 @@ namespace TinderCloneV1
         }
 
         [FunctionName("GetUser")]
-        public async Task<HttpResponseMessage> GetUser([HttpTrigger(AuthorizationLevel.Anonymous, 
-        "get", "post", Route = "student/{ID}")] HttpRequestMessage req, HttpRequest request, int ID, ILogger log){
+        public async Task<HttpResponseMessage> GetUser([HttpTrigger(AuthorizationLevel.Anonymous,
+        "get", "post", Route = "student/{ID}")] HttpRequestMessage req, HttpRequest request, int ID, ILogger log)
+        {
 
             UserService userService = new UserService(req, request, log);
 
-            using (SqlConnection connection = new SqlConnection(str)) {
-                try{
+            using (SqlConnection connection = new SqlConnection(str))
+            {
+                try
+                {
                     connection.Open();
                 }
-                catch (SqlException e){
+                catch (SqlException e)
+                {
                     log.LogInformation(e.Message);
                     return exceptionHandler.ServiceUnavailable(log);
                 }
 
                 // [Studentdata]
                 // GET a student by his ID.
-                if (req.Method == HttpMethod.Get){
+                if (req.Method == HttpMethod.Get)
+                {
                     httpResponseMessage = userService.GetStudent(ID, connection);
-                } 
+                }
 
-                // [Studentdata]
-                // PUT: Update a student by his ID.
-                else if (req.Method == HttpMethod.Put) {
+                //// [Studentdata]
+                //// PUT: Update a student by his ID.
+                else if (req.Method == HttpMethod.Put)
+                {
                     httpResponseMessage = userService.PutStudent(ID, connection);
                 }
 
                 connection.Close();
-            } 
+            }
             return await httpResponseMessage;
         }
     }
