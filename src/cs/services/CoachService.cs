16949b5127975c77c0df45e54c9c413910295649
MikeStrijbis <56072067+MikeStrijbis@users.noreply.github.com>
2019-10-24 14:29:28 +0200

    Added GetCoachByID
    Added GetCoachByID call, updated YAML. TODO: Test call

--- src/cs/services/CoachService.cs
+++ src/cs/services/CoachService.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Net.Http;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace TinderCloneV1 {
     class CoachService : ICoachService {
+        private readonly string environmentString = Environment.GetEnvironmentVariable("sqldb_connection");
 
         private readonly HttpRequestMessage req;
         private readonly HttpRequest request;
@@ -28,17 +37,59 @@ namespace TinderCloneV1 {
         public Task<HttpResponseMessage> GetAllCoaches() {
             throw new NotImplementedException();
         }
-
+        
         public Task<HttpResponseMessage> GetCoachAndWorkloadByID(int coachID) {
             throw new NotImplementedException();
         }
 
         public Task<HttpResponseMessage> GetCoachByID(int coachID) {
-            throw new NotImplementedException();
+            ExceptionHandler exceptionHandler = new ExceptionHandler(coachID);
+            Coach newCoach;
+
+            string queryString = $"SELECT * FROM [dbo].[Coach] WHERE studentID = {coachID};";
+            log.LogInformation($"Executing the following query: {queryString}");
+
+            try {
+                using (SqlConnection connection = new SqlConnection(environmentString)) {
+                    try {
+                        connection.Open();
+
+                        using (SqlCommand command = new SqlCommand(queryString, connection)) {
+                            using (SqlDataReader reader = command.ExecuteReader()) {
+                                if (!reader.HasRows) {
+                                    return exceptionHandler.NotFoundException(log);
+                                } else {
+                                    while (reader.Read()) {
+                                        newCoach = new Coach {
+                                            studentID = reader.GetInt32(0),
+                                            workload = reader.GetInt32(1)
+                                        };
+                                    }
+                                }
+                            }
+                        }
+                    } catch (SqlException e) {
+                        log.LogError(e.Message);
+                        return exceptionHandler.ServiceUnavailable(log);
+                    } finally {
+                        connection.Close();
+                    }
+                }
+            } catch (SqlException e) {
+                log.LogError(e.Message);
+                return exceptionHandler.BadRequest(log);
+            }
+
+            var jsonToReturn = JsonConvert.SerializeObject(newCoach);
+            log.LogInformation($"{HttpStatusCode.OK} | Data shown succesfully");
+
+            return new HttpResponseMessage(HttpStatusCode.OK) {
+                Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
+            };
         }
 
         public Task<HttpResponseMessage> UpdateCoachAndWorkloadByID(int coachID) {
-            throw new NotImplementedException();
+                throw new NotImplementedException();
+            }
         }
     }
-}
