b6d8cdf15ddcd3fe982c475ccfad76912a27ba06
MikeStrijbis <56072067+MikeStrijbis@users.noreply.github.com>
2019-10-25 00:46:20 +0200

    Working on Coach Services
    Added DeleteCoachByID, updated YAML  and added CoachProfile class

--- src/cs/services/CoachService.cs
+++ src/cs/services/CoachService.cs
@@ -26,70 +26,152 @@ namespace TinderCloneV1 {
             this.log = log;
         }
 
-        public Task<HttpResponseMessage> CreateCoach() {
+        public async Task<HttpResponseMessage> CreateCoach() {
+            ExceptionHandler exceptionHandler = new ExceptionHandler(0);
+            Coach newCoach = new Coach();
+            User newUser = new User();
+            JObject jObject = new JObject();
+
+            string body = await req.Content.ReadAsStringAsync();
+
             throw new NotImplementedException();
+
         }
 
-        public Task<HttpResponseMessage> DeleteCoachByID(int coachID) {
-            throw new NotImplementedException();
+        public async Task<HttpResponseMessage> DeleteCoachByID(int coachID) {
+            ExceptionHandler exceptionHandler = new ExceptionHandler(coachID);
+
+            //Query string used to delete the coach from the coach table
+            string queryString_Coach = $@"DELETE
+                                            FROM [dbo].[Coach]
+                                            WHERE studentID = @coachID";
+
+            //Query string used to delete the coach from the Students table
+            string queryString_Student = $@"DELETE
+                                            FROM [dbo].[Student]
+                                            WHERE studentID = @coachID";
+
+            try {
+                using (SqlConnection connection = new SqlConnection(environmentString)) {
+                    try {
+                        //The connection is automatically closed when going out of scope of the using block
+                        connection.Open();
+
+                        //Delete the coach from the Coach table
+                        using (SqlCommand command = new SqlCommand(queryString_Coach, connection)) {
+                            //Parameters are used to ensure no SQL injection can take place
+                            command.Parameters.Add("@coachID", System.Data.SqlDbType.Int).Value = coachID;
+                            log.LogInformation($"Executing the following query: {queryString_Coach}");
+
+                            command.ExecuteNonQuery();
+                        }
+
+                        //Delete the profile from the Students table
+                        using (SqlCommand command = new SqlCommand(queryString_Student, connection)) {
+                            //Parameters are used to ensure no SQL injection can take place
+                            command.Parameters.Add("@coachID", System.Data.SqlDbType.Int).Value = coachID;
+                            log.LogInformation($"Executing the following query: {queryString_Student}");
+
+                            command.ExecuteNonQuery();
+                        }
+                    } catch (SqlException e) {
+                        //Return response code 503
+                        log.LogError(e.Message);
+                        return exceptionHandler.ServiceUnavailable(log);
+                    }
+                }
+            } catch (SqlException e) {
+                //Return response code 400
+                log.LogError(e.Message);
+                return exceptionHandler.BadRequest(log);
+            }
+
+            log.LogInformation($"{HttpStatusCode.NoContent} | Data deleted succesfully");
+
+            //Return response code 204
+            return new HttpResponseMessage(HttpStatusCode.NoContent);
         }
 
         public Task<HttpResponseMessage> GetAllCoaches() {
             throw new NotImplementedException();
         }
-        
+
         public Task<HttpResponseMessage> GetCoachAndWorkloadByID(int coachID) {
             throw new NotImplementedException();
         }
 
-        public Task<HttpResponseMessage> GetCoachByID(int coachID) {
+        //Returns the profile of the coach (from the student table) 
+        //and the workload of the coach (from the coach table)
+        public async Task<HttpResponseMessage> GetCoachByID(int coachID) {
             ExceptionHandler exceptionHandler = new ExceptionHandler(coachID);
-            Coach newCoach;
+            CoachProfile newCoachProfile = new CoachProfile();
 
-            string queryString = $"SELECT * FROM [dbo].[Coach] WHERE studentID = {coachID};";
-            log.LogInformation($"Executing the following query: {queryString}");
+            string queryString = $@"SELECT Student.*, Coach.workload
+                                    FROM [dbo].[Coach], [dbo].[Student]
+                                    WHERE Coach.studentID = @coachID AND Student.studentID = @coachID;";
 
             try {
                 using (SqlConnection connection = new SqlConnection(environmentString)) {
                     try {
+                        //The connection is automatically closed when going out of scope of the using block
                         connection.Open();
 
                         using (SqlCommand command = new SqlCommand(queryString, connection)) {
+                            //Parameters are used to ensure no SQL injection can take place
+                            command.Parameters.Add("@coachID", System.Data.SqlDbType.Int).Value = coachID;
+                            log.LogInformation($"Executing the following query: {queryString}");
+
                             using (SqlDataReader reader = command.ExecuteReader()) {
                                 if (!reader.HasRows) {
+                                    //Return response code 404
                                     return exceptionHandler.NotFoundException(log);
                                 } else {
                                     while (reader.Read()) {
-                                        newCoach = new Coach {
-                                            studentID = reader.GetInt32(0),
-                                            workload = reader.GetInt32(1)
-                                        };
+                                        newCoachProfile = new CoachProfile(
+                                            new Coach {
+                                                studentID = reader.GetInt32(0),
+                                                workload = reader.GetInt32(10)
+                                            },
+                                            new User {
+                                                studentID = reader.GetInt32(0),
+                                                firstName = reader.GetString(1),
+                                                surName = reader.GetString(2),
+                                                phoneNumber = reader.GetString(3),
+                                                photo = reader.GetString(4),
+                                                description = reader.GetString(5),
+                                                degree = reader.GetString(6),
+                                                study = reader.GetString(7),
+                                                studyYear = reader.GetInt32(8),
+                                                interests = reader.GetString(9)
+                                            }
+                                        );
                                     }
                                 }
                             }
                         }
                     } catch (SqlException e) {
+                        //Return response code 503
                         log.LogError(e.Message);
                         return exceptionHandler.ServiceUnavailable(log);
-                    } finally {
-                        connection.Close();
                     }
                 }
             } catch (SqlException e) {
+                //Return response code 400
                 log.LogError(e.Message);
                 return exceptionHandler.BadRequest(log);
             }
 
-            var jsonToReturn = JsonConvert.SerializeObject(newCoach);
+            var jsonToReturn = JsonConvert.SerializeObject(newCoachProfile);
             log.LogInformation($"{HttpStatusCode.OK} | Data shown succesfully");
 
+            //Return response code 200 and the requested data
             return new HttpResponseMessage(HttpStatusCode.OK) {
                 Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
             };
         }
 
         public Task<HttpResponseMessage> UpdateCoachAndWorkloadByID(int coachID) {
-                throw new NotImplementedException();
-            }
+            throw new NotImplementedException();
         }
     }
+}
